27591065e853ab58a31ca2359a4c6626ff80134d
joellozanobarbancho <joellozanobarbancho@elpuig.xeill.net>
2025-03-05 19:55:03 +0100

    4 new methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        method1();
+        method2();
+        method3();
+        method4();
+    }
+    public static void method1() {
+
+    }
+    public static void method2() {
+
+    }
+    public static void method3() {
+
+    }
+    public static void method4() {
+
     }
 }
